8a26d97f1a8280405768a350fb4066b535b56b41
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-07-10 00:02:01 +0100

    First version 0.1.0 of CostInflator.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import hello from './hello';
-import {
-  getPeeps,
-  getStaff,
-  getHandymen,
-  getMechanics,
-  getSecurity,
-  getEntertainers,
-} from './helpers';
-
 const main = (): void => {
-  console.log(`${hello()} Your plug-in has started!`);
+  console.log('Your plug-in has started!');
 
-  console.log(`In your park, there are currently ${getPeeps().length} peeps`);
-  console.log(`${getStaff().length} of them is your staff.`);
+  const enabled = true;
+  const outgoingsMultiplier = 1.0;
+  const incomeMultiplier = 1.0;
 
-  console.log('Your staff consists of:');
-  console.log(`- ${getHandymen().length} handymen`);
-  console.log(`- ${getMechanics().length} mechanics`);
-  console.log(`- ${getSecurity().length} security`);
-  console.log(`- ${getEntertainers().length} entertainers`);
+  if (enabled) {
+    console.log(`Outgoings will be multiplied by ${outgoingsMultiplier}`);
+    console.log(`Income will be multiplied by ${incomeMultiplier}`);
+  }
+
+  const handlePayment = (e: PaymentEventArgs): void => {
+    if (enabled) {
+      if (e.amount > 0) e.amount *= outgoingsMultiplier;
+      if (e.amount < 0) e.amount *= incomeMultiplier;
+    }
+  };
+
+  context.subscribe('park.finance.payment', handlePayment);
 };
 
 export default main;
