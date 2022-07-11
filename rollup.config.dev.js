7b5ab263fe8f2bfe9971c9df48668f7fe82832ab
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-07-11 23:41:54 +0100

    Updated NPM configuration

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,3 +1,4 @@
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
@@ -8,6 +9,7 @@ export default {
     format: 'iife',
   },
   plugins: [
+    resolve(),
     typescript(),
     terser({
       format: {
