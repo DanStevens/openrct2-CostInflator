59ae9a8ba82f05e9cd25347337c9ae4007e42be9
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-07-19 18:45:27 +0100

    Removed tab from settings window

--- src/ui.ts
+++ src/ui.ts
@@ -28,76 +28,71 @@ export class SettingsWindow {
     width: 200,
     height: 120,
     onClose: () => this.window = null,
-    tabs: [
+    widgets: [
       {
-        image: 0, // TODO
-        widgets: [
-          {
-            name: "EnabledCheckbox",
-            type: "checkbox",
-            x: 10,
-            y: 50,
-            width: 60,
-            height: 15,
-            text: "Enabled",
-            isChecked: this.settings.enabled,
-            onChange: (isChecked) => this.settings.enabled = isChecked,
-          },
-          {
-            name: "RideRunningCostsLabel",
-            type: "groupbox",
-            x: 5,
-            y: 70,
-            width: 190,
-            height: 45,
-            text: "Ride Running Costs",
-          },
-          {
-            name: "MultiplierLabel",
-            type: "label",
-            x: 15,
-            y: 90,
-            width: 60,
-            height: 15,
-            text: "Multiplier",
-          },
-          {
-            name: "RideUpkeepMultiplierValue",
-            type: "spinner",
-            x: 80,
-            y: 88,
-            width: 70,
-            height: 15,
-            text: this.settings.rideUpkeepMultiplier.toFixed(2),
-            onIncrement: () => {
-              if (this.settings.rideUpkeepMultiplier < 99.99)
-                this.settings.rideUpkeepMultiplier =
-                  increment(this.settings.rideUpkeepMultiplier, 0.01);
+        name: "EnabledCheckbox",
+        type: "checkbox",
+        x: 10,
+        y: 50,
+        width: 60,
+        height: 15,
+        text: "Enabled",
+        isChecked: this.settings.enabled,
+        onChange: (isChecked) => this.settings.enabled = isChecked,
+      },
+      {
+        name: "RideRunningCostsLabel",
+        type: "groupbox",
+        x: 5,
+        y: 70,
+        width: 190,
+        height: 45,
+        text: "Ride Running Costs",
+      },
+      {
+        name: "MultiplierLabel",
+        type: "label",
+        x: 15,
+        y: 90,
+        width: 60,
+        height: 15,
+        text: "Multiplier",
+      },
+      {
+        name: "RideUpkeepMultiplierValue",
+        type: "spinner",
+        x: 80,
+        y: 88,
+        width: 70,
+        height: 15,
+        text: this.settings.rideUpkeepMultiplier.toFixed(2),
+        onIncrement: () => {
+          if (this.settings.rideUpkeepMultiplier < 99.99)
+            this.settings.rideUpkeepMultiplier =
+              increment(this.settings.rideUpkeepMultiplier, 0.01);
+          this.syncWithSettings();
+        },
+        onDecrement: () => {
+          if (this.settings.rideUpkeepMultiplier > 0)
+            this.settings.rideUpkeepMultiplier =
+              decrement(this.settings.rideUpkeepMultiplier, 0.01);
+          this.syncWithSettings();
+        },
+        onClick: () => {
+          this.ui.showTextInput({
+            title: "Enter new value",
+            description: "Enter new value",
+            initialValue: this.rideUpkeepMultiplierSpinner.text,
+            callback: value => {
+              const num = parseFloat(value);
+              this.settings.rideUpkeepMultiplier =
+                num < 0 ? 0 : (num > 99.99 ? 99.99 : num);
               this.syncWithSettings();
             },
-            onDecrement: () => {
-              if (this.settings.rideUpkeepMultiplier > 0)
-                this.settings.rideUpkeepMultiplier =
-                  decrement(this.settings.rideUpkeepMultiplier, 0.01);
-              this.syncWithSettings();
-            },
-            onClick: () => {
-              this.ui.showTextInput({
-                title: "Enter new value",
-                description: "Enter new value",
-                initialValue: this.rideUpkeepMultiplierSpinner.text,
-                callback: value => {
-                  const num = parseFloat(value);
-                  this.settings.rideUpkeepMultiplier =
-                    num < 0 ? 0 : (num > 99.99 ? 99.99 : num);
-                  this.syncWithSettings();
-                },
-              });
-            }
-          }
-        ]
+          });
+        }
       }
-    ],
+    ]
   };
 
   private syncWithSettings() {
