68f2f101dcf4c4f5b4fbb68ac3114e36270ae85a
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-07-31 16:23:24 +0100

    Initial 'sanity test' version of plugin that writes a log message

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -1,10 +1,10 @@
-/// <reference path="../lib/openrct2.d.ts" />
+/// <reference path="../../OpenRCT2.repo/distribution/openrct2.d.ts" />
 
 import main from './main';
 
 registerPlugin({
   name: 'CostInflator',
-  version: '1.0',
+  version: '0.1.0',
   authors: ['DanStevens'],
   type: 'local',
   licence: 'MIT',
